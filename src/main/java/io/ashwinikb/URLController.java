98f4f02ed4dd22e6cefedf1d5f3439a7b8b54d12
ashwinikb <ashwini.kbasavaraju@gmail.com>
2016-12-14 14:56:34 -0800

    Adding changes done for project

--- src/main/java/io/ashwinikb/URLController.java
+++ src/main/java/io/ashwinikb/URLController.java
@@ -2,18 +2,33 @@ package io.ashwinikb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class URLController {
 
-	
-    @RequestMapping("/short-url")
-    public String shortURL(Model model) {
-//        model.addAttribute("name", name);
 
-    	return "shortURL";
+	@RequestMapping("/shorturl")
+    public String shortURL(Model model) {
+        model.addAttribute("url", new Url());
+        return "shortURL";
     }
+	
+	@RequestMapping("/short-this-url")
+    public String shortThisURL(@ModelAttribute Url url) {
+        return "results";
+    }
+	
+
+//    @PostMapping("/shorturl")
+//    public String urlSubmit(@ModelAttribute Url url) {
+//        return "result";
+//    }
 
 }
---
+++ src/main/java/io/ashwinikb/Url.java
@@ -0,0 +1,16 @@
+package io.ashwinikb;
+
+public class Url {
+	
+	    private String content;
+
+	    public String getContent() {
+	        return content;
+	    }
+
+	    public void setContent(String content) {
+	        this.content = content;
+	    }
+
+
+}
